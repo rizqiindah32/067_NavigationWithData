9d5d2f5e276c83beca6986c52660ede257cb2af2
rizqiindah32 <147020631+rizqiindah32@users.noreply.github.com>
2023-11-15 23:59:00 +0700

    Menambahkan string

--- app/src/main/java/com/example/fore/data/OrderUiState.kt
+++ app/src/main/java/com/example/fore/data/OrderUiState.kt
@@ -4,4 +4,8 @@ data class OrderUiState(
     val jumlah: Int = 0,
     val rasa: String = "",
     val harga: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val umur: String = "",
+    val pekerjaan : String ="",
 )
